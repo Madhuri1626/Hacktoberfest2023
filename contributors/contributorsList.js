fb0f7f5af97f55c0d86b90b8ccd3227cd32aecd7
GUNDABATTULA MADHURI <114327324+Madhuri1626@users.noreply.github.com>
2023-10-21 18:49:46 +0530

    hacktoberfest

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Madhuri G
+    username: "https://github.com/Madhuri1626",
+  },
 ];
